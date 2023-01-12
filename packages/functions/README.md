f6186301ec06862fa2667e51621f53e692b85eab
Hlib Spychak <dingolediego@gmail.com>
2023-01-12 11:27:07 +0000

    Deleted all packages, libs and tools; created functions package and sdk lib

---
+++ packages/functions/README.md
@@ -0,0 +1,3 @@
+# Functions
+
+Yehaaaw
---
+++ tools/install_fission_cli.sh
@@ -0,0 +1,2 @@
+curl -Lo fission https://github.com/fission/fission/releases/download/v1.17.0/fission-v1.17.0-linux-amd64 \
+    && chmod +x fission && sudo mv fission /usr/local/bin/
